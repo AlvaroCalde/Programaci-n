6737813177aa3845c003e6ba54416d122a317c68
Álvaro Calderón Pérez <alvaro.calper@educa.jcyl.es>
2025-01-24 09:33:59 +0100

    Delete MiPrimerPrograma.java

--- MiPrimerPrograma.java
+++
@@ -1,8 +0,0 @@
-/*Realiza el pseudocódigo y la codificación en lenguaje de programación Java de un
-algoritmo que muestre por pantalla el siguiente mensaje: Mi primer programa.
-*/
-public class MiPrimerPrograma{
-    public static void main(String[] args){
-	  System.out.println("MI primer programa");
-	}
-}
