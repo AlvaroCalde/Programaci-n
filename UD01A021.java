a10d462fde49b9525fc343efb5b5af31a714e335
Álvaro Calderón Pérez <alvaro.calper@educa.jcyl.es>
2025-01-24 09:34:34 +0100

    Delete UD01A021.java

--- UD01A021.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class UD01A021{
-	public static void main(String[] args){
-		Scanner teclado=new Scanner(System.in);
-		String nombre;
-		nombre=teclado.nextLine();
-		System.out.println("Hola " +nombre);
-	}
-}
